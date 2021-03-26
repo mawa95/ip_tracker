e776ab85ff684d73f3e76253196470b5fadfc071
Martyna <mawa95@gmail.com>
2021-03-26 09:45:01 +0100

    react leaflet

--- ip-app/src/index.js
+++ ip-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; //entrypoint  injecting js
 import './index.scss';
+import 'leaflet/dist/leaflet.css';
 import Input from './input';
 import Info from './info';
 import Map from './map';
